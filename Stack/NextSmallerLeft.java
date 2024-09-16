3131ed9b37de58b0b3ef04b41de667e31c10a064
Rishu Chauhan <rishuchauhan>
2024-09-16 10:25:16 +0530

    nxtSmallerLeft

---
+++ Stack/NextSmallerLeft.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class NextSmallerLeft {
+    public static void main(String[] args) {
+        int nums[]={6,8,0,1,3};
+        int nxtGreater []=new int [nums.length];
+        Stack<Integer> s=new Stack<>();
+        for(int i=0;i<nums.length;i++){
+        while(!s.isEmpty() && nums[s.peek()]>=nums[i]){
+            s.pop();
+        }
+        if(s.isEmpty()){
+            nxtGreater[i]=-1;
+        }
+        else{
+            nxtGreater[i]=nums[s.peek()];
+        }
+        s.push(i);
+        }
+
+        for(int i=0;i<nxtGreater.length;i++){
+        System.out.print(nxtGreater[i]+" ");
+        }
+        System.out.println();
+    }
+}
